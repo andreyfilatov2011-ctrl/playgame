3a9930468491d448cab882297df1b95971b78ad7
Pro <mvke_money@mail.ru>
2026-06-10 09:24:04 +0300

    gravity and border

--- player.py
+++ player.py
@@ -1,8 +1,7 @@
 import pygame
-
 from condfig import SPEED, WIDTH, HEIGHT
 
-GRAVITY = 1
+GRAVITY = 0.8  # лучше 0.8, чем 1 (более плавно)
 
 
 class Player(pygame.sprite.Sprite):
@@ -15,29 +14,32 @@ class Player(pygame.sprite.Sprite):
         self.velocity = 0
 
     def update(self):
+        # Гравитация
         self.velocity += GRAVITY
         self.rect.y += self.velocity
 
-
+        # Горизонтальное движение
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.rect.x -= SPEED
         if keys[pygame.K_RIGHT]:
             self.rect.x += SPEED
 
+        # Границы по горизонтали
         if self.rect.left > WIDTH:
             self.rect.right = 0
-
         if self.rect.right < 0:
-            self.rect.left = 800
+            self.rect.left = WIDTH
 
-
+        # Граница по земле (остановка падения)
         if self.rect.bottom >= HEIGHT:
             self.rect.bottom = HEIGHT
+            self.velocity = 0  # на земле скорость обнуляем
 
     def jump(self):
-        self.rect.y -= 100
-        self.velocity = 0
+        # Прыжок только с земли
+        if self.rect.bottom >= HEIGHT:
+            self.velocity = -18  # отрицательная скорость = прыжок вверх
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
